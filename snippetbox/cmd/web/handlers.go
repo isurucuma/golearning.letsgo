a908c9ed7effdc0e884eeb09059cd4673783e457
isuru <isurucuma@gmail.com>
2024-05-25 23:24:56 +0530

    databse connections done upto insert

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -45,11 +45,18 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		// w.WriteHeader(http.StatusMethodNotAllowed)
-		// w.Write([]byte(http.StatusText(http.StatusMethodNotAllowed)))
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.WriteHeader(201)
-	w.Write([]byte("snippet created..."))
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("snippet/view?id=%d", id), http.StatusSeeOther)
 }
--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"isurucuma.golearning.letsgo/internal/models"
+
+	_ "github.com/go-sql-driver/mysql" // this is aliazed with _ because there is no place in this file we use the driver specific thing
+	// but still we want to run the init method in the driver so that it can register itself with the databse/sql package
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
+
+const (
+	username = "root"
+	password = "password"
+	hostname = "localhost:3306"
+	dbName   = "snippetbox"
+)
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network port")
+
+	defaultDsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", username, password, hostname, dbName)
+
+	dsn := flag.String("dsn", defaultDsn, "MySQL data source name")
+
 	flag.Parse()
 
 	f, err := os.OpenFile("./tmp/info.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -26,8 +46,17 @@ func main() {
 
 	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errLog.Fatal(err)
+	}
+	infoLog.Print("Database connection successfull")
+	defer db.Close()
+
 	app := &application{
-		errLog, infoLog,
+		errorLog: errLog,
+		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	infoLog.Print("Server starting on", *addr)
@@ -41,4 +70,16 @@ func main() {
 	err = server.ListenAndServe()
 
 	errLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
